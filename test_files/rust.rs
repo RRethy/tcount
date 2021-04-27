b2127cff7f3ff2ba19fbbac03003beb5d0a85ce6
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-04-27 13:57:28 -0400

    Move main.rs code around a bit

--- test_files/rust.rs
+++ test_files/rust.rs
@@ -1,5 +1,4 @@
 use std::collections::BTreeMap;
-use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
 
@@ -11,23 +10,23 @@ mod parser;
 use count::count_tokens;
 use error::Error;
 
-fn counts(files: Vec<PathBuf>) -> BTreeMap<parser::Language, u64> {
-    files
+// foobar baz
+fn run(cli: cli::Cli) -> Result<(), Error> {
+    let (parsed, failed): (Vec<_>, Vec<_>) = cli // foobarbaz
+        .files
         .into_iter()
         .map(|path| match parser::parse(&path) {
             Ok((tree, lang)) => Ok((lang, count_tokens(&tree))),
             Err(err) => Err(err),
         })
-        .filter(Result::is_ok)
+        .partition(Result::is_ok);
+    parsed
+        .into_iter()
         .map(Result::unwrap)
         .fold(BTreeMap::new(), |mut acc, (lang, count)| {
             *acc.entry(lang).or_insert(0) += count;
             acc
         })
-}
-
-fn run(cli: cli::Cli) -> Result<(), Error> {
-    counts(cli.files)
         .iter()
         .for_each(|(lang, count)| println!("{:?}: {}", lang, count));
     Ok(())
@@ -43,9 +42,4 @@ fn main() {
         }
         _ => {}
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 }
