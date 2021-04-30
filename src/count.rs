e6d81896edb79c40aef6b8976452598b08428b59
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-04-30 13:06:15 -0400

    Add --kind to count specific kinds of nodes

--- src/count.rs
+++ src/count.rs
@@ -1,17 +1,14 @@
 use tree_sitter::Tree;
 
-pub fn count_tokens(tree: &Tree) -> u64 {
-    let mut ntokens = 0;
+pub fn count(tree: &Tree, counters: &mut Vec<Box<dyn Counter>>) {
     let mut cursor = tree.walk();
 
-    // preoder traversal of the concrete syntax tree
+    // preoder traversal of the syntax tree
     'outer: loop {
         let node = cursor.node();
-        // TODO handle comments via cli maybe
-        if node.child_count() == 0 && !node.is_extra() && !node.is_missing() {
-            // TODO what if this is not a terminal node
-            ntokens += 1;
-        }
+        counters
+            .iter_mut()
+            .for_each(|counter| counter.report(&node));
         if !cursor.goto_first_child() && !cursor.goto_next_sibling() {
             'inner: loop {
                 if !cursor.goto_parent() {
@@ -23,5 +20,59 @@ pub fn count_tokens(tree: &Tree) -> u64 {
             }
         }
     }
-    ntokens
+}
+
+pub trait Counter {
+    fn report(&mut self, node: &tree_sitter::Node);
+    fn get_count(&self) -> u64;
+    fn reset(&mut self);
+}
+
+pub struct KindCounter {
+    kind: String,
+    n: u64,
+}
+
+impl KindCounter {
+    pub fn new(kind: String) -> Self {
+        KindCounter { kind, n: 0 }
+    }
+}
+
+impl Counter for KindCounter {
+    fn report(&mut self, node: &tree_sitter::Node) {
+        if self.kind == node.kind() && !node.is_missing() {
+            self.n += 1;
+        }
+    }
+    fn get_count(&self) -> u64 {
+        self.n
+    }
+    fn reset(&mut self) {
+        self.n = 0;
+    }
+}
+
+pub struct TokenCounter {
+    n: u64,
+}
+
+impl TokenCounter {
+    pub fn new() -> Self {
+        TokenCounter { n: 0 }
+    }
+}
+
+impl Counter for TokenCounter {
+    fn report(&mut self, node: &tree_sitter::Node) {
+        if node.child_count() == 0 && !node.is_extra() && !node.is_missing() {
+            self.n += 1;
+        }
+    }
+    fn get_count(&self) -> u64 {
+        self.n
+    }
+    fn reset(&mut self) {
+        self.n = 0;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,64 @@ mod count;
 mod error;
 mod parser;
 
-use count::count_tokens;
+use count::{count, Counter, KindCounter, TokenCounter};
 use error::Error;
+use parser::Language;
 
-fn counts(files: &Vec<PathBuf>) -> (BTreeMap<parser::Language, u64>, Vec<Error>) {
-    let (parsed, errors): (Vec<_>, Vec<_>) = files
+fn make_counters(kinds: &Vec<String>) -> Vec<Box<dyn Counter>> {
+    let mut counters: Vec<Box<dyn Counter>> = Vec::new();
+    counters.push(Box::new(TokenCounter::new()));
+    kinds
         .iter()
-        .map(|path| match parser::parse(&path) {
-            Ok((tree, lang)) => Ok((lang, count_tokens(&tree))),
-            Err(err) => Err(err),
+        .for_each(|kind| counters.push(Box::new(KindCounter::new(kind.clone()))));
+    counters
+}
+
+fn get_counts(
+    files: &Vec<PathBuf>,
+    kinds: &Vec<String>,
+) -> (Vec<(Language, Vec<u64>)>, Vec<Error>) {
+    let mut counters = make_counters(&kinds);
+    let (counts, errors): (Vec<_>, Vec<_>) = files
+        .iter()
+        .map(|path| {
+            counters.iter_mut().for_each(|counter| counter.reset());
+            let (tree, lang) = parser::parse(&path)?;
+            count(&tree, &mut counters);
+            Ok((
+                lang,
+                counters.iter().map(|counter| counter.get_count()).collect(),
+            ))
         })
         .partition(Result::is_ok);
-    let counts =
-        parsed
-            .into_iter()
-            .map(Result::unwrap)
-            .fold(BTreeMap::new(), |mut acc, (lang, count)| {
-                *acc.entry(lang).or_insert(0) += count;
-                acc
-            });
-    (counts, errors.into_iter().map(Result::unwrap_err).collect())
+    (
+        counts.into_iter().map(Result::unwrap).collect(),
+        errors.into_iter().map(Result::unwrap_err).collect(),
+    )
+}
+
+fn group_by_language(counts: Vec<(Language, Vec<u64>)>) -> BTreeMap<parser::Language, Vec<u64>> {
+    counts
+        .into_iter()
+        .fold(BTreeMap::new(), |mut acc, (lang, counts)| {
+            if let Some(cur) = acc.get_mut(&lang) {
+                cur.iter_mut()
+                    .zip(&counts)
+                    .for_each(|(cum, val)| *cum += val);
+            } else {
+                acc.insert(lang, counts);
+            }
+            acc
+        })
 }
 
 fn run(cli: cli::Cli) -> Result<(), Error> {
-    let (langs, errors) = counts(&cli.files);
-    langs
+    let (counts, errors) = get_counts(&cli.files, &cli.kind);
+    let lang_counts = group_by_language(counts);
+    lang_counts
         .iter()
-        .for_each(|(lang, count)| println!("{:?}: {}", lang, count));
+        .for_each(|(lang, count)| println!("{:?}: {:?}", lang, count));
+
     if cli.verbose {
         // TODO print this nicer
         eprintln!("{:?}", errors);
@@ -64,8 +95,10 @@ mod tests {
             PathBuf::from("./test_data/rust2.rs"),
         ];
         let mut expected = BTreeMap::new();
-        expected.insert(parser::Language::Rust, 35 + 20);
-        let (got, _) = counts(&files);
+        expected.insert(parser::Language::Rust, vec![35 + 20]);
+        let kinds = vec![];
+        let (got, _) = get_counts(&files, &kinds);
+        let got = group_by_language(got);
         assert_eq!(got, expected);
     }
 }
