0e0cd4ce79e9280b6781909ae22f1f4712394056
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-05-08 17:53:27 -0400

    Add tests for queries

--- src/query.rs
+++ src/query.rs
@@ -1,11 +1,12 @@
 use crate::error::Result;
 use crate::language::Language;
 use std::collections::HashMap;
+use std::env::var;
 use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum QueryKind {
     Match,
     Captures(Vec<String>),
@@ -30,30 +31,35 @@ impl FromStr for Query {
     ///
     /// A query directory is defined as a directory with subdirectories that contain query files
     /// with the name "{query name}.scm". These subdirectories are named based on the language
-    /// those queries are written for. For example, a query directory queries/ could have a
+    /// those queries are written for. For example, a query directory tc_queries/ could have a
     /// "comments" query for rust and ruby named queries/rust/comments.scm and
     /// queries/ruby/comments.scm, respectively.
     ///
-    /// First, the present working directory is searched for a query directory named tc_queries/,
-    /// then $XDG_CONFIG_HOME/tc (defaults to $HOME/.config/tc) is searched and each subdirectory
-    /// is considered a query directory and searched (conflicting query files result in undefined
-    /// behaviour), lastly the builtin queries directory is searched.
+    /// When searching for these query files, the following order is take. First, the present
+    /// working directory is searched for a query directory named .tc_queries/, then
+    /// $XDG_CONFIG_HOME/tc (defaults to $HOME/.config/tc) is searched for query directories
+    /// that match $XDG_CONFIG_HOME/tc/* (conflicting query files result in undefined behaviour),
+    /// lastly the builtin queries directory is searched.
     fn from_str(name: &str) -> std::result::Result<Self, Self::Err> {
         let queries: Option<HashMap<Language, tree_sitter::Query>> = vec![
-            // look in pwd for a tc_queries/ dir
-            format!("tc_queries/*/{}.scm", name),
+            // look in pwd for a .tc_queries/ dir
+            format!(".tc_queries/*/{}.scm", name),
             // look in $XDG_CONFIG_HOME/tc/* for a dir with queries
             format!(
                 "{}/tc/*/*/{}.scm",
-                if env!("XDG_CONFIG_HOME").len() > 0 {
-                    env!("XDG_CONFIG_HOME").to_string()
+                if var("XDG_CONFIG_HOME").unwrap_or(String::new()).len() > 0 {
+                    var("XDG_CONFIG_HOME").unwrap().to_string()
                 } else {
-                    format!("{}/{}", env!("HOME"), ".config")
+                    format!("~/{}", ".config")
                 },
                 name
             ),
-            // look in the root of this repo for it's queries/ dir
-            format!("{}/queries/*/{}.scm", env!("CARGO_MANIFEST_DIR"), name),
+            // look in the root of this repo for it's builtin_queries/ dir
+            format!(
+                "{}/builtin_queries/*/{}.scm",
+                var("CARGO_MANIFEST_DIR").unwrap_or(String::new()),
+                name
+            ),
         ]
         .iter()
         .map(|dir_glob| glob::glob(dir_glob.as_str()))
@@ -91,5 +97,85 @@ impl FromStr for Query {
         } else {
             Err(String::from("TODO"))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env::set_var;
+
+    fn setup() {
+        set_var(
+            "XDG_CONFIG_HOME",
+            format!("{}/test_data/xdg_config_home", env!("CARGO_MANIFEST_DIR")),
+        );
+        set_var(
+            "CARGO_MANIFEST_DIR",
+            format!(
+                "{}/test_data/cargo_manifest_dir",
+                env!("CARGO_MANIFEST_DIR")
+            ),
+        );
+    }
+
+    #[test]
+    fn test_query_in_pwd() {
+        setup();
+        let query = Query::from_str("_test").unwrap();
+        assert_eq!("_test", query.name);
+        assert_eq!(QueryKind::Match, query.kind);
+        assert_eq!(
+            ["pwd.test"],
+            query.langs.get(&Language::Go).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["pwd.test"],
+            query.langs.get(&Language::Ruby).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["pwd.test"],
+            query.langs.get(&Language::Rust).unwrap().capture_names()
+        );
+    }
+
+    #[test]
+    fn test_query_in_xdg_config_home() {
+        setup();
+        let query = Query::from_str("string").unwrap();
+        assert_eq!("string", query.name);
+        assert_eq!(QueryKind::Match, query.kind);
+        assert_eq!(
+            ["xdg.string"],
+            query.langs.get(&Language::Go).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["xdg.string"],
+            query.langs.get(&Language::Ruby).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["xdg.string"],
+            query.langs.get(&Language::Rust).unwrap().capture_names()
+        );
+    }
+
+    #[test]
+    fn test_query_in_manifest_dir() {
+        setup();
+        let query = Query::from_str("false").unwrap();
+        assert_eq!("false", query.name);
+        assert_eq!(QueryKind::Match, query.kind);
+        assert_eq!(
+            ["manifest.false"],
+            query.langs.get(&Language::Go).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["manifest.false"],
+            query.langs.get(&Language::Ruby).unwrap().capture_names()
+        );
+        assert_eq!(
+            ["manifest.false"],
+            query.langs.get(&Language::Rust).unwrap().capture_names()
+        );
     }
 }
---
+++ test_data/xdg_config_home/tc/queries/go/string.scm
@@ -0,0 +1,1 @@
+(raw_string_literal) @xdg.string
